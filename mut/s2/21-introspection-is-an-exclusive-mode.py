4e82f7d60a4dff65a594506b0e675ad9d4d84700
neriumpete <pavlovskipetko@protonmail.com>
2026-09-10 22:20:32 +0300

    re-anchor two mutations on the response key

--- mut/s2/21-introspection-is-an-exclusive-mode.py
+++ mut/s2/21-introspection-is-an-exclusive-mode.py
@@ -1,10 +1,10 @@
 # Return early on the first introspection root, dropping every other root field from the response.
 p = "src/serve.rs"; s = open(p).read()
-old = """                data.insert("__schema".into(), d["__schema"].clone());
+old = """                data.insert(root.key.clone(), d["__schema"].clone());
                 continue;"""
+assert s.count(old) == 1, s.count(old)
 new = """                return (
                     StatusCode::OK,
                     Json(serde_json::json!({"data": {"__schema": d["__schema"].clone()}})),
                 );"""
-assert s.count(old) == 1
 open(p, "w").write(s.replace(old, new))
